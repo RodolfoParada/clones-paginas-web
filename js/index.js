604004f82fe65d0453b4a012cd1de43ddae349bd
Rodolfo Parada <rodolfo.parada.gonzalez>
2025-03-05 18:04:06 -0300

    mejora funcion

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 function redireccionar(select) {
     if (select.value !== "") {
-        var win = window.open(select.value, '_blank');
-        win.focus();
+        try {
+            var win = window.open(select.value, '_blank');
+            if (win) {
+                win.focus(); // Esto da foco a la nueva pestaña
+            } else {
+                alert('La ventana emergente fue bloqueada. Intenta habilitar pop-ups.');
+            }
+        } catch (e) {
+            alert('Error al intentar abrir la ventana emergente: ' + e.message);
+        }
     }
 }
