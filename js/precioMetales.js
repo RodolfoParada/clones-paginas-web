270441f5f6c0af992846c2cea0eccc77fe7cbb86
Rodolfo Parada <rodolfo.parada.gonzalez>
2025-03-12 20:26:01 -0300

    precioMetales

---
+++ js/precioMetales.js
@@ -0,0 +1,9 @@
+
+//   document.addEventListener("DOMContentLoaded", function () {
+//     let myCarousel = new bootstrap.Carousel(document.getElementById("#carouselExampleCaptions"), {
+//       interval: 3000, // Velocidad de cambio (3 segundos)
+//       wrap: true,     // Permitir que el carrusel vuelva al primer slide
+//       ride: "carousel"
+//     });
+//   });
+
